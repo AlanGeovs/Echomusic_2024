ef82f8b2968155895c3514596c6bdf1cb2115e0e
AlanGeovs <alan@gnesys.com>
2023-04-26 08:46:12 -0500

    Primera carga del Repositorio EchoMusic 2023

---
+++ model/db.php
@@ -0,0 +1,9 @@
+<?php
+/* conexion a la base de datos*/
+    define('DB_HOST', 'localhost');
+    define('DB_USER', 'genesysa_echomisuc');
+    define('DB_PASS', 'Y8vJ2.0Iw_Le');
+    define('DB_NAME', 'genesysa_echomisuc');
+ 
+ 
+?>
