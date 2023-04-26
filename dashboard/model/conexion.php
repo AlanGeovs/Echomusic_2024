84e012e67a35f1130360fcb03c87ca22f8205b66
AlanGeovs <alan@gnesys.com>
2023-04-26 16:05:44 -0500

    Carga Módulo Dashboard

--- dashboard/model/conexion.php
+++ dashboard/model/conexion.php
@@ -3,7 +3,7 @@
 class Conexion{
 	public static function conectar(){
 		//$link= new PDO("mysql:host=localhost; dbname=trademar_tmk","root","");
-		$link= new PDO("mysql:host=localhost; dbname=am_inventariov2","am_inventariov2","Y)])HDIHRo&p");
+		$link= new PDO("mysql:host=localhost; dbname=genesysa_echomisuc","genesysa_echomisuc","Y8vJ2.0Iw_Le");
 		return $link;
 	} 
 }
